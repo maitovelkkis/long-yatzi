c418b13aec2e483614cf47e1796075d89f5d737a
Perttu Männistö <perttu.mannisto@hotmail.com>
2020-11-24 18:41:54 +0200

    Add Dice.cs class

---
+++ LongYatzi/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongYatzi
+{
+    class Dice
+    {
+        private int _diceAmount = 6;
+        public List<int> ThrowDice()
+        {
+            List<int> _diceList = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i < _diceAmount; i++)
+            {
+                _diceList.Add(rand.Next(1, 6));
+            }
+            return _diceList;
+
+        }
+    }
+}
